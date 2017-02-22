ace007210976015a6c9a96ee6475b65222a9f6a9
yes.heng <yes.heng@icloud.com>
2017-02-22 23:40:41 +0800

    add post view

--- routes/post.js
+++ routes/post.js
@@ -4,17 +4,22 @@ const router = express.Router();
 
 /* GET posts page. */
 router.get('/', (req, res) => {
-  res.render('post');
+  res.render('posts');
+});
+
+// get create post page
+router.get('/create', (req, res) => {
+  res.render('create_post');
 });
 
 // create post
 router.post('/', (req, res) => {
-  res.render('post');
+  res.render('create_post');
 });
 
 // edit post
 router.put('/:id/edit', (req, res) => {
-  res.render('post');
+  res.render('edit_post');
 });
 
 module.exports = router;
