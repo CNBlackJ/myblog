4650b7caf0d7417032b23fb6d63e1ea6deb7002a
CNBlackJ <yes.heng@icloud.com>
2017-03-01 23:29:47 +0800

    Add delete logic

--- routes/post.js
+++ routes/post.js
@@ -45,4 +45,11 @@ router.put('/:id/edit', (req, res) => {
   res.render('edit_post');
 });
 
+// delete post
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  await Post.findOne({ _id: id }).remove().exec();
+  res.redirect('/posts');
+});
+
 module.exports = router;
